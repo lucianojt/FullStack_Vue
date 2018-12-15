e1fadfeb019f8df698bcd470810f6fc06833c931
jidapa <onjidapa@windowslive.com>
2018-12-15 15:58:14 +0700

    put API

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <div class="nav navbar-nav">
+      <router-link to="/" class="nav-item nav-link active">Home</router-link> |
+      <router-link to="/about" class="nav-item nav-link">About</router-link> |
+      <router-link to="/Categories" class="nav-item nav-link">Categories</router-link> |
+    
+    </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Categories.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Product list</h1>
+    <b-table striped hover :items="categories" :fields="fields" :per-page = "pageSize" :current-page = "pageIndex"></b-table>
+    <b-pagination size="md" :total-rows="categories.length" v-model="pageIndex" :per-page="pageSize">
+    </b-pagination>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'categories',
+  
+  data(){
+      return{
+          message:'project2',
+          categories: [],
+          pageSize: 10,
+          pageIndex: 1,
+          fields: [{
+            key:'category_id',
+            sortable : true
+          },
+          {
+              key:'category_name',
+            sortable : true
+          },
+          {
+              key:'description',
+            sortable : true,
+            variant: 'danger'
+          }]
+      }
+  },
+  mounted(){
+      var instance = this
+      axios
+      .get('https://finalfull2018.herokuapp.com/api/categories')
+      .then(function(response){
+          console.log(response.data)
+          instance.categories = response.data.data
+      })
+    }
+}
+</script>
